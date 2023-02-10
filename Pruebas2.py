4b6b873e8d75b124b1cd491c3dbf105e3958eb7d
DomKiwi <dominkiwi98@gmail.com>
2023-02-11 00:29:22 +0100

    Extracting Relevant data from previous data
    Obtencion de diferentes datos utiles para calculos posteriores, se añadiran nuevos como probabilidades, desviaciones, etc

--- Pruebas2.py
+++ Pruebas2.py
@@ -22,16 +22,27 @@ class Equipo:       #Clasificacion de Equipos con sus respectivos datos de inter
     puntosEmpatados = 0
     golesEncontraUltimasJornadas = 0
     golesAfavorUltimasJornadas = 0
+    maximosGoles = 0
+    minimosGoles = 0
+    maximosGolesEnContra = 0
+    minimosGolesEnContra = 0
+    mediaGolPorPartidoMarcados = 0
+    mediaGolPorPartidoEnContra = 0
 
     def getDataframe(self):         #de los valores que estoy trabajando, los meto en un dataframe para manejarlos mejor
         recopilacionequipo = [self.columnaPosiciones, self.name,
                               self.puntos, self.partidos, self.wins, self.draws, self.loses,  self.afavor, self.encontra, self.equiposQueHaGanado,self.equiposQueHaPerdido,
-                              self.equiposQueHaEmpatado, self.golesAfavorUltimasJornadas, self.golesEncontraUltimasJornadas,
+                              self.equiposQueHaEmpatado, self.golesAfavorUltimasJornadas, self.golesEncontraUltimasJornadas, self.maximosGoles, self.minimosGoles,
+                              self.maximosGolesEnContra, self.minimosGolesEnContra, self.mediaGolPorPartidoMarcados,
+                              self.mediaGolPorPartidoEnContra
                               ]
         recopilacionequipo = pd.DataFrame(recopilacionequipo).transpose()
         return recopilacionequipo
         
-    def __init__(self, columnasPosiciones, name, puntos, partidos, wins, draws, loses, afavor, encontra, ganados, perdidos, empatados, golesAfavorUltimasJornadas, golesEncontraUltimasJornadas):     #Llamo a los datos que me interan ya inicializados y les añado el valor que he dado en la entrada de la clase
+    def __init__(self, columnasPosiciones, name, puntos, partidos, wins,
+                 draws, loses, afavor, encontra, ganados, perdidos, empatados,
+                 golesAfavorUltimasJornadas, golesEncontraUltimasJornadas, maximosGoles, minimosGoles,
+                 maximosGolesEnContra,minimosGolesEnContra, mediaGolPorPartidoMarcados, mediaGolPorPartidoEnContra):     #Llamo a los datos que me interan ya inicializados y les añado el valor que he dado en la entrada de la clase
         self.columnaPosiciones = columnasPosiciones
         self.name = name
         self.puntos = puntos
@@ -46,7 +57,12 @@ class Equipo:       #Clasificacion de Equipos con sus respectivos datos de inter
         self.equiposQueHaEmpatado = empatados
         self.golesAfavorUltimasJornadas = golesAfavorUltimasJornadas
         self.golesEncontraUltimasJornadas = golesEncontraUltimasJornadas
-
+        self.maximosGoles = maximosGoles
+        self.minimosGoles = minimosGoles
+        self.maximosGolesEnContra = maximosGolesEnContra
+        self.minimosGolesEnContra = minimosGolesEnContra
+        self.mediaGolPorPartidoMarcados = mediaGolPorPartidoMarcados
+        self.mediaGolPorPartidoEnContra = mediaGolPorPartidoEnContra
 
 page = requests.get('https://www.resultados-futbol.com/primera')
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -55,9 +71,7 @@ blockquote_items = soup.find('table', {'id': 'tabla2'})
 blockquote_items = blockquote_items.find('tbody')
 blockquote_items = blockquote_items.find_all('tr')
 listaDeEquipos = []
-header = ["posicion", "nombre", "puntos", "partidos", "ganados", "empatados", "perdidos", "afavor", "encontra", "Ha Ganado vs", "Ha perdido vs", "Ha empatado vs", "golesAfavorUltimas5", "golesEnContraUltimas5"]
-dataframetotal = pd.DataFrame(header).transpose()
-# print(dataframetotal)
+dataframetotal = pd.DataFrame()
 
 for num, blockquote in enumerate(blockquote_items):
     #con cada equipo poner su fila en horizontal todos los datos, escribo todas las opciones para que recoga los datos
@@ -78,6 +92,10 @@ for num, blockquote in enumerate(blockquote_items):
     equiposQueHaEmpatado =[]
     golesAfavorUltimasJornadas = 0
     golesEncontraUltimasJornadas = 0
+    minimosGoles = 100
+    maximosGoles = 0
+    minimosGolesEnContra = 100
+    maximosGolesEnContra = 0
     for resultado in resultadopartidos:
         resultadoDelPartido = resultado.find("li", {"class": "title g"}).contents[0]
         equiposDelPartido = resultado.find_all("b", {"class": "bname"})
@@ -87,6 +105,12 @@ for num, blockquote in enumerate(blockquote_items):
         golesDelPartido = resultado.find_all("b", {"class": "bres"})
         golesDeAmbos = [b.get_text() for b in golesDelPartido]
         equiposYgoles = dict(map(lambda i, j: (i, j), ambosEquipos, golesDeAmbos))
+
+        if int(equiposYgoles[equipo])>maximosGoles: maximosGoles = int(equiposYgoles[equipo])
+        if int(equiposYgoles[equipo])<minimosGoles: minimosGoles = int(equiposYgoles[equipo])
+        if int(equiposYgoles[equipoContrario[0]])>maximosGolesEnContra: maximosGolesEnContra = int(equiposYgoles[equipoContrario[0]])
+        if int(equiposYgoles[equipoContrario[0]])<minimosGolesEnContra: minimosGolesEnContra = int(equiposYgoles[equipoContrario[0]])
+
         golesAfavorUltimasJornadas = golesAfavorUltimasJornadas + int(equiposYgoles[equipo])
         golesEncontraUltimasJornadas = golesEncontraUltimasJornadas + int(equiposYgoles[equipoContrario[0]])
 
@@ -97,8 +121,15 @@ for num, blockquote in enumerate(blockquote_items):
         if resultadoDelPartido == 'EMPATE':
             equiposQueHaEmpatado.append(equipoContrario[0])
 
+    numDePartidos = len(equiposQueHaGanado) + len(equiposQueHaperdido) + len(equiposQueHaEmpatado)
+    mediaGolPorPartidoMarcados = golesAfavorUltimasJornadas/numDePartidos
+    mediaGolPorPartidoEnContra = golesEncontraUltimasJornadas/numDePartidos
 
-    Data = Equipo(columnaPosiciones, equipo, puntos, partidos, wins, draws, loses, afavor, encontra, equiposQueHaGanado, equiposQueHaperdido, equiposQueHaEmpatado, golesAfavorUltimasJornadas, golesEncontraUltimasJornadas)
+    Data = Equipo(columnaPosiciones, equipo, puntos, partidos, wins, draws, loses,
+                  afavor, encontra, equiposQueHaGanado, equiposQueHaperdido, equiposQueHaEmpatado,
+                  golesAfavorUltimasJornadas, golesEncontraUltimasJornadas, maximosGoles, minimosGoles,
+                  maximosGolesEnContra,minimosGolesEnContra, mediaGolPorPartidoMarcados, mediaGolPorPartidoEnContra
+                  )
     recopilacionDeDatos = Data.getDataframe()
     dataframetotal = pd.concat([dataframetotal,recopilacionDeDatos])
     listaDeEquipos.append(Data)
@@ -106,8 +137,21 @@ for num, blockquote in enumerate(blockquote_items):
 
 
 
-# for equipo in listaDeEquipos:
-# print(pd.DataFrame(listaDeEquipos))
-# print(pd.DataFrame.from_dict(Puntos, orient='index', columns=['Puntos']))
+
+header = ["posicion", "nombre", "puntos", "partidos", "ganados", "empatados",
+          "perdidos", "afavor", "encontra", "Ha Ganado vs", "Ha perdido vs",
+          "Ha empatado vs", "golesAfavorUltimas5", "golesEnContraUltimas5", "maximos goles marcados ultimos5", "minimos goles marcados ultimos5", "maximos goles en contra ultimo5", "minimos goles en contra ultimos5",
+          "media gol marcado ultimos5", "media gol en contra ultimos5"]
+dataframetotal.columns = header
 print(dataframetotal.to_string())
-# dataframetotal.to_excel("output.xlsx", sheet_name = "Datos en vivo", index=0, header=0)
+
+dataframetotal.to_excel("output.xlsx", sheet_name = "Datos en vivo", index=0)
+
+
+
+
+
+#Implementar:
+    #Grafica por cada quiepo con medias y desviaciones
+    #añadir metodologias de probabilidad para los datos recogidos
+    #
