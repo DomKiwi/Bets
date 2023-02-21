cafa03c19116c72a510252e2f977cdb051909796
DomKiwi <dominkiwi98@gmail.com>
2023-02-21 15:49:28 +0100

    Thinking about

--- Pruebas1.py
+++ Pruebas1.py
@@ -16,20 +16,9 @@ from vincent.colors import brews
 class Equipo:
     # Agrego valores iniciales de los datos que me interesan de la pagina web como otros que quiera crear yo nuevos
     name = ""
-    columnaPosiciones = 0
-    puntos = 0
-    partidos = 0
-    wins = 0
-    loses = 0
-    draws = 0
-    afavor = 0
-    encontra = 0
     equiposQueHaGanado = []
     equiposQueHaPerdido = []
     equiposQueHaEmpatado = []
-    puntosGanados = 0
-    puntosPerdidos = 0
-    puntosEmpatados = 0
     golesEncontraUltimasJornadas = 0
     golesAfavorUltimasJornadas = 0
     maximosGoles = 0
@@ -38,38 +27,26 @@ class Equipo:
     minimosGolesEnContra = 0
     mediaGolPorPartidoMarcados = 0
     mediaGolPorPartidoEnContra = 0
-    # puntosDeFuerza = len(equiposQueHaGanado)
     puntosDeDebilidad = 0
     puntosDeFuerza = 0
     equiposQueHaEmpatadoNombre = []
     equiposQueHaGanadoNombre = []
     equiposQueHaPerdidoNombre = []
 
-    def __init__(self, name, columnasPosiciones=0, puntos=0, partidos=0, wins=0,
-                 draws=0, loses=0, afavor=0, encontra=0, ganados=[], perdidos=[], empatados=[],
+    def __init__(self, name, ganados=[], perdidos=[], empatados=[],
                  golesAfavorUltimasJornadas=0, golesEncontraUltimasJornadas=0, maximosGoles=0, minimosGoles=0,
                  maximosGolesEnContra=0, minimosGolesEnContra=0, mediaGolPorPartidoMarcados=0,
                  mediaGolPorPartidoEnContra=0):  # Llamo a los datos que me interan ya inicializados y les añado el valor que he dado en la entrada de la clase
         self.name = name
-        self.setvalues(columnasPosiciones, puntos, partidos, wins,
-                       draws, loses, afavor, encontra, ganados, perdidos, empatados,
+        self.setvalues(ganados, perdidos, empatados,
                        golesAfavorUltimasJornadas, golesEncontraUltimasJornadas, maximosGoles, minimosGoles,
                        maximosGolesEnContra, minimosGolesEnContra, mediaGolPorPartidoMarcados,
                        mediaGolPorPartidoEnContra)
 
-    def setvalues(self, columnasPosiciones=0, puntos=0, partidos=0, wins=0,
-                  draws=0, loses=0, afavor=0, encontra=0, ganados=[], perdidos=[], empatados=[],
+    def setvalues(self,ganados=[], perdidos=[], empatados=[],
                   golesAfavorUltimasJornadas=0, golesEncontraUltimasJornadas=0, maximosGoles=0, minimosGoles=0,
                   maximosGolesEnContra=0, minimosGolesEnContra=0, mediaGolPorPartidoMarcados=0,
                   mediaGolPorPartidoEnContra=0):  # Llamo a los datos que me interan ya inicializados y les añado el valor que he dado en la entrada de la clase
-        self.columnaPosiciones = columnasPosiciones
-        self.puntos = puntos
-        self.partidos = partidos
-        self.wins = wins
-        self.loses = loses
-        self.draws = draws
-        self.afavor = afavor
-        self.encontra = encontra
         self.equiposQueHaGanado = ganados
         self.equiposQueHaPerdido = perdidos
         self.equiposQueHaEmpatado = empatados
@@ -90,15 +67,7 @@ class Equipo:
     def getDataframe(self):  # de los valores que estoy trabajando, los meto en un dataframe para manejarlos mejor
         self.transformData()
         recopilacionequipo = [
-            self.columnaPosiciones,
             self.name,
-            self.puntos,
-            self.partidos,
-            self.wins,
-            self.draws,
-            self.loses,
-            self.afavor,
-            self.encontra,
             self.equiposQueHaGanadoNombre,
             self.equiposQueHaPerdidoNombre,
             self.equiposQueHaEmpatadoNombre,
@@ -130,15 +99,7 @@ class Equipo:
         recopilacionImportanteEquipo = pd.DataFrame(recopilacionImportanteEquipo).transpose()
 
         recopilacionequipo.columns = [
-            "Position",
             "Team",
-            "Points",
-            "Matches",
-            "Wins",
-            "Draws",
-            "Loses",
-            "G_Scored",
-            "G_Against",
             "Won vs",
             "Lost vs",
             "Draw vs",
@@ -168,46 +129,65 @@ class Equipo:
         ]
         return recopilacionequipo, recopilacionImportanteEquipo
 
-def getTeamsObjects(soupOfTeams):
-    listOfTeams = []
-    tableData = soupOfTeams.find('table', {'class': "styled__TableStyled-sc-43wy8s-1 iOBNZZ"})
-    eachRowMatch = tableData.find_all('div', {'class': 'styled__MatchStyled-sc-2hkd8m-1 jVNhaC'})
-    for row in eachRowMatch:
-        results = row.find_all('p', {'class': 'styled__TextRegularStyled-sc-1raci4c-0 fYuQIM'})
-        teams = row.find_all('p', {'class': 'styled__TextRegularStyled-sc-1raci4c-0 hvREvZ'})
-        lenOfTeams = len(teams)
-        for i in range(lenOfTeams):
-            # print(teams[i].contents[0], results[i].contents[0])
-            teamClass = Equipo(teams[i].contents[0])
-            listOfTeams.append(teamClass)
-        # for t in teams:
-        #     listOfTeams.append(t.contents[0])
-        #     print(t.contents[0])
-        # # goals = soupOfTeams.find_all('p', {'class': 'styled__TextRegularStyled-sc-1raci4c-0 fYuQIM'})
-        # for r in results:
-        #     g = r.find_all('p', {'class': 'styled__TextRegularStyled-sc-1raci4c-0 fYuQIM'})
-        #     for gg in g:
-        #         print(gg.contents[0])
-        # break
-
-    # for team in currentDay:
-    #     teamObject = Equipo(team.contents[0])
-    #     listOfTeams.append(teamObject)
-    return listOfTeams
-
 def getEquipo(teamName, listaDeEquipos):
     for b in listaDeEquipos:
         if b.name == teamName:
             return b
+###################################################################################################################################################################
 
-# route = requests.get('https://www.laliga.com/laliga-santander/resultados')  # Get URL
-route = requests.get('https://www.laliga.com/laliga-santander/resultados/2022-23/jornada-20')  # Get URL
+def getTeamsObjects(route):
+    listOfTeams = []
+    tableData = route.find('table', {'class': "styled__TableStyled-sc-43wy8s-1 iOBNZZ"})
+    eachRowMatch = tableData.find_all('div', {'class': 'styled__MatchStyled-sc-2hkd8m-1 jVNhaC'})
+    resultadosPartidos = []
+    for row in eachRowMatch:
+        match = {}
+        results = row.find_all('p', {'class': 'styled__TextRegularStyled-sc-1raci4c-0 fYuQIM'})
+        teams = row.find_all('p', {'class': 'styled__TextRegularStyled-sc-1raci4c-0 hvREvZ'})
+        lenOfTeams = len(teams)
+        for i in range(lenOfTeams):
+            match[teams[i].contents[0]] =  results[i].contents[0]
+            teamClass = Equipo(teams[i].contents[0])
+            listOfTeams.append(teamClass)
+        resultadosPartidos.append(match)
+    return listOfTeams
+###################################################################################################################################################################
 
+def getPreviousJourneys(route, listOfTeams, itermax):
+    #busco la jornada actual
+    journeys = soupOfTeams.find('div', {'class': 'styled__SubHeaderCalendarGrid-sc-1engvts-8 iYpljZ'}).find('div', {'class': 'styled__DropdownContainer-d9k1bl-0 kmhQzc'})
+    currentJourney = journeys.find('span').contents[0]
+    #busco las jornadas
+    eachjourney = journeys.find('ul', {'class': 'styled__ItemsList-d9k1bl-2 hkGQnA'}).find_all('a')
+    lenJourneys = len(eachjourney)
+    previousJourneys = []
+    linkPreviousJourneys = []   #uso esto para buscar cada jornada
+    for numday, day in enumerate(eachjourney):
+        linkPreviousJourneys.append(day['href'])
+        previousJourneys.append(day)
+        if day.contents[0] == currentJourney:
+            break
+
+    enterPage = requests.get('https://www.laliga.com/' + linkPreviousJourneys[0])  # Get URL
+
+
+
+
+
+
+###################################################################################################################################################################
+route = requests.get('https://www.laliga.com/laliga-santander/resultados')  # Get URL
 soupOfTeams = BeautifulSoup(route.text, 'html.parser')  # Use this format to read it
 
-#For day BASE (obtener), -i to iter (4,5,6,7...) result efficiency
+###################################################################################################################################################################
 
+#For day BASE (obtener), -i to iter (4,5,6,7...) result efficiency
+# Set inicial
 listOfPrimera = getTeamsObjects(soupOfTeams)
+itermax = 12
+getPreviousJourneys(route, listOfPrimera, itermax)
+###################################################################################################################################################################
+
 #obtener modulo de partidos de X jornada:
-for i in listOfPrimera:
-    print(i.name)
+# for i in listOfPrimera:
+#     print(i.name)
